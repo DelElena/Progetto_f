d2bbe5c7637e06767fe62f5bae63875005939a43
Elena Del Puente <area.ena@gmail.com>
2022-05-18 08:03:31 +0100

    Forza

--- JS/allinea.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('#mostra4').allinea();
-function allinea(click) {
-    document.getElementsByClassName('.contenitore').style.display = "grid";
-
-
-}
-
--- JS/carosel.js
+++
@@ -1,8 +0,0 @@
-const { Carousel } = require("bootstrap")
-
-document.addEventListener("click", function () {
-    const carousel = new Carousel("carousel-item", {
-        direction: 'horizontal',
-        loop: true
-    });
-});
